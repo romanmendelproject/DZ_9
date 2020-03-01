4449645c276c55edfc6b3ccdb0138a5a9c973480
romanmendelproject <33754158+romanmendelproject@users.noreply.github.com>
2020-03-02 00:45:27 +0300

    new

--- myproject/coursesapi/schema.py
+++ myproject/coursesapi/schema.py
@@ -4,63 +4,13 @@ from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 
-
-
-
-# from .models import School, City
-#
-#
 class TeacherType(DjangoObjectType):
     class Meta:
         model = Teacher
 
-#
-# class TeacherFilteredType(DjangoObjectType):
-#
-#     class Meta:
-#         model = Teacher
-#         filter_fields = {
-#             'name': ['exact', 'icontains', 'istartswith'],
-#         }
-#         interfaces = (graphene.relay.Node, )
-#
-#
-# # class CityType(DjangoObjectType):
-# #
-# #     class Meta:
-# #         model = City
-#
-#
-# class TeacherMutation(graphene.Mutation):
-#
-#     class Arguments:
-#         school_id = graphene.Int(required=True)
-#         new_name = graphene.String(required=True)
-#
-#     result = graphene.Boolean()
-#     teacher = graphene.Field(SchoolType)
-#
-#     def mutate(self, info, school_id, new_name):
-#         # TODO
-#         return {
-#             'result': True,
-#             'school': Teacher.objects.first()
-#         }
-#
-#
-# class Mutation:
-#     change_teacher_name = TeacherMutation.Field()
-#
-#
+
 class Query:
     all_teacher = graphene.List(TeacherType, limit=graphene.Int())
-#     filtered_teachers = DjangoFilterConnectionField(TeacherFilteredType)
-#     retrieve_teacher = graphene.Field(TeacherType, id=graphene.Int())
-#
-#     def resolve_all_schools(self, *args, **kwargs):
-#         if 'limit' in kwargs:
-#             return Teacher.objects.all()[:kwargs['limit']]
-#         return Teacher.objects.all()
-#
+
     def resolve_all_teacher(self, info, **kwargs):
         return Teacher.objects.all()
